1e29aae1f8e532009d2a96c8f5c8a410927ac4ae
masud45827 <60808203+masud45827@users.noreply.github.com>
2022-08-25 04:38:35 -0700

    Create phi function(coprime calculate.cpp

---
+++ phi function(coprime calculate.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef     long long    ll;
+typedef     unsigned long long    ull;
+typedef     vector<int> vi;
+typedef     vector<long long> vl;
+typedef     pair<int, int>pi;
+typedef     pair<long long, long long>pl;
+#define F   first
+#define S   second
+#define pb  push_back
+#define     all(x)      x.begin() , x.end()
+#define mod 1000000007
+#define       FOR(i,a,b) for(i=a;i<=b;i++)
+#define     mem(a)      memset(a , 0 ,sizeof a)
+#define     memn(a)     memset(a , -1 ,sizeof a)
+#ifdef LOCAL
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+void err(istream_iterator<string> it) {cout << endl ;}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+    cerr << *it << " = " << a << ' ' ;
+    err(++it, args...);
+}
+#else
+#define error(args...)
+#endif
+int phi(int n) {
+    int result = n;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            while (n % i == 0){
+                n /= i;
+              }
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+int main() {
+    int test = 1, fac = 1, n;
+    // scanf("%d", &test);
+    while (1) {
+        cin >> n;
+        if (n == 0) break;
+        cout << phi(n) << endl;
+    }
+    return 0;
+}
+
